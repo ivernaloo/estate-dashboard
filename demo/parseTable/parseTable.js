14f32e84b7e5b9f0607f538a677a4c82eacdfdd8
aloo <aloocs@gmail.com>
2017-03-30 18:37:17 +0800

    optimize the code

--- demo/parseTable/parseTable.js
+++ demo/parseTable/parseTable.js
@@ -5,11 +5,11 @@
  * @return {Array} the converted object
  */
 function arrayify(collection, range) {
-  var _array = Array.prototype.slice.call(collection);
-  if (range){
-    _array = _array.slice(range[0], range[1])
-  }
-  return _array; // simple convert array-like to array
+    var _array = Array.prototype.slice.call(collection);
+    if (range) {
+        _array = _array.slice(range[0], range[1])
+    }
+    return _array; // simple convert array-like to array
 }
 
 /**
@@ -19,44 +19,35 @@ function arrayify(collection, range) {
  * @return {Function}      a function that takes a table row and spits out an object
  */
 function factory(headings) {
-  return function(row) {
-    return arrayify(row.cells).reduce(function(prev, curr, i) {
-      prev[headings[i]] = curr.innerText;
-      return prev;
-    }, {});
-  }
+    return function (row) {
+        return arrayify(row.cells).reduce(function (prev, curr, i) {
+            prev[headings[i]] = curr.innerText;
+            return prev;
+        }, {});
+    }
 }
 
 /**
  * given a table, generate an array of objects.
  * each object corresponds to a row in the table.
  * each object's key/value pairs correspond to a column's heading and the row's value for that column
- * 
+ *
  * @param  {HTMLTableElement} table the table to convert
  * @return {Array[Object]}    array of objects representing each row in the table
  */
 function parseTable(table) {
-  var headings = []; // custom headings
+    var headings = [],
+        h1s      = arrayify(table.rows[0].cells),
+        h2s      = arrayify(table.rows[1].cells); // custom headings
 
-  var h1s = arrayify(table.rows[0].cells).map(function(heading, index) {   // get the 1st rows get the column title
-    console.log("heading : ", heading, index)
-    return heading.innerText; // extract the title
-  });
-  var h2s = arrayify(table.rows[1].cells).map(function(heading) {   // get the 1st rows get the column title
-    return heading.innerText; // extract the title
-  });
+    // custom the title and combine two line to one line title
+    h1s.forEach(function (heading, index) {   // get the 1st rows get the column title
+        if (index < 1) { // exclude first title
+            headings.push(heading.innerText);
+            return;
+        }
+        headings.push(heading.innerText + "-" + h2s[2 * (index - 1)].innerText, heading.innerText + "-" + h2s[2 * index - 1].innerText);
+    });
 
-  h1s.forEach(function(key, i){
-
-    if (i < 1 ) { // exclude first title
-      headings.push(key);
-      return;
-    }
-
-    headings.push(h1s[i] + "-" + h2s[2*(i-1)]);
-    headings.push(h1s[i] + "-" + h2s[2*i -1]);
-
-  });
-
-  return arrayify(table.tBodies[0].rows, [2,-2]).map(factory(headings)); // extract the content
+    return arrayify(table.tBodies[0].rows, [2, -2]).map(factory(headings)); // extract the content
 }
