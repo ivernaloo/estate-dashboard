ef1eaf62b6a8b92b9c1c16ea0d7b14dcd53a9486
aloo <aloocs@gmail.com>
2017-03-31 08:48:03 +0800

    add mongo manipulate syntax

--- demo/mongodriver/app.js
+++ demo/mongodriver/app.js
@@ -7,9 +7,11 @@ MongoCli.connect(url, function(err, db){
     assert.equal(null, err);
     console.log("connected correctly to server");
 
+    // insertDocuments(db, function(res){
+    //     db.close();
+    //     console.log(res)
+    // })
 
-    insertDocuments(db, console.log)
-    db.close();
 });
 
 var insertDocuments = function(db, callback) {
@@ -44,3 +46,42 @@ var insertDocuments = function(db, callback) {
     });
 }
 
+
+var updateDocument = function(db, callback) {
+    // Get the documents collection
+    var collection = db.collection('documents');
+    // Update document where a is 2, set b equal to 1
+    collection.updateOne({ a : 2 }
+        , { $set: { b : 1 } }, function(err, result) {
+            assert.equal(err, null);
+            assert.equal(1, result.result.n);
+            console.log("Updated the document with the field a equal to 2");
+            callback(result);
+        });
+}
+
+
+var deleteDocument = function(db, callback) {
+    // Get the documents collection
+    var collection = db.collection('documents');
+    // Insert some documents
+    collection.deleteOne({ a : 3 }, function(err, result) {
+        assert.equal(err, null);
+        assert.equal(1, result.result.n);
+        console.log("Removed the document with the field a equal to 3");
+        callback(result);
+    });
+}
+
+var findDocuments = function(db, callback) {
+    // Get the documents collection
+    var collection = db.collection('documents');
+    // Find some documents
+    collection.find({}).toArray(function(err, docs) {
+        assert.equal(err, null);
+        assert.equal(2, docs.length);
+        console.log("Found the following records");
+        console.dir(docs);
+        callback(docs);
+    });
+}
