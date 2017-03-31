80493c84f5dfd182906163e53ab0d28692f8f269
aloo <aloocs@gmail.com>
2017-03-31 17:42:05 +0800

    tunning the require path

--- crawler/task/index.js
+++ crawler/task/index.js
@@ -1,12 +1,8 @@
 var debug = require("debug"),
-    cron = require('node-cron'),
-    crawler = require("../../crawler/db/crawler.js").init,
-    format = require("../../crawler/db/transform.js").format;
+    root = process.cwd(),
+    crawler = require(root + "/crawler/db/crawler.js");
+
 
 var log = debug("crontab");
-
 log(".................")
-cron.schedule('* */6 * * *', function(){
-    console.log("start a new crawler");
-    crawler(format);
-});
+
