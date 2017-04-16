6f34d40ea6596d1088784235ff876ea73854fd55
aloo <aloocs@gmail.com>
2017-04-16 18:38:39 +0800

    add api service logic

---
+++ api/index.js
@@ -0,0 +1,16 @@
+/*
+* provide api service for web
+* 2017-04-15
+* */
+
+var debug = require('debug'),
+    log = debug("API : "),
+    config   = require("config"),
+    root     = process.cwd(),
+    database = require(root + "/crawler/database/mongo");
+
+
+database.queryDistrict();
+
+log("START...");
+
