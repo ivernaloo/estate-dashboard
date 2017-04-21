a7f9f5ec39d9120d2eb0cb76b91c4083d00423cb
aloo <aloocs@gmail.com>
2017-04-21 15:46:32 +0800

    refactor the checkupdate logic

--- crawler/task/latest.js
+++ crawler/task/latest.js
@@ -18,73 +18,63 @@ log = debug("latest : ");
 // @done get the latest
 // @done
 function checkUpdate(success, failure) {
-    var log = debug("checkUpdate : "),
-        queue = [];
+    var log = debug("checkUpdate : ");
 
     log("start");
     database.findLatest(function (latest) { // find storage lastest
-        crawler.parseList(URL, function (items, next) {
-            var date;
-
-            // check the first item date
-            items.length > 0
-                ? date = items[0].children[0].data.replace(/\D+/g, " ").split(" ").slice(0, 3).join("/")
-                : log("items crawl err");
-
-            if (date.indexOf("/") != 4 || date.split("/").length != 3) {
-                log("something wrong in date get : ", date);
-                log("something wrong in date get : ", date.indexOf("/"));
-                log("something wrong in date get : ", date.indexOf("/") != 4);
-                log("something wrong in date get : ", date.split("/").length != 3);
-                return; // no date and jump from the source
-            }
-
-            // recursive from here
-            // @todo concurrence to async queue. this iterate should transform into async queue, but not concurrence
-            /*
-            * return the array contain element lists
-            * */
-            var _temp = items.filter(function (item, index) {
-                var url = item.attribs.href,
-                    // reference : http://stackoverflow.com/questions/10003683/javascript-get-number-from-string
-                    date = item.children[0].data.replace(/\D+/g, " ").split(" ").slice(0, 3).join("/"); // should jump when unormal info
-                // log("Date compare : ", new Date(date) > new Date(latest), date, latest);
-
-                // need prevent date get prolem, such as notice which have no relationship with the data
-                // jump from none data source
-                // fixed bug: http://scxx.whfcj.gov.cn/scxxbackstage/whfcj/channels/854_4.html
-                // http://scxx.whfcj.gov.cn/scxxbackstage/whfcj/contents/854/24309.html
-                // 有最新日期，并且抓取到的日期不大于最新日期的时候，跳出循环
-                if (date.split("/").length != 3) date = 0;
-                log("index : ", index);
-                return new Date(date) > new Date(latest);
-/*
-                if () {
-                    log("start crawl");
-                    queue.push({date: date, url: url});
-                    success(date, url)
-
-
-                } else {
-                    // has updated to the latest items
-                    failure && failure();
-                    log("stop crawl, no new info", date, latest);
-                    return true
-                }
-*/
-            });
-
-            log("filter array : ", _temp)
-            log("filter array : ", _temp.length)
-
-            log("next : ", next);
-            log("Queue : ", queue);
-            log("items length : ", items.length);
-            log("Queue length: ", queue.length);
-        }, true);
+        crawler.parseList(URL,function(items, next){
+            buildUpdateCollection(items, next, latest)
+        });
     });
 }
 
+function buildUpdateCollection(items, next, latest, queue){
+            var date,
+                _queue = queue || [];
+
+        // check the first item date
+        items.length > 0
+            ? date = items[0].children[0].data.replace(/\D+/g, " ").split(" ").slice(0, 3).join("/")
+            : log("items crawl err");
+
+        if (date.indexOf("/") != 4 || date.split("/").length != 3) {
+            log("something wrong in date get : ", date);
+            log("something wrong in date get : ", date.indexOf("/"));
+            log("something wrong in date get : ", date.indexOf("/") != 4);
+            log("something wrong in date get : ", date.split("/").length != 3);
+            return; // no date and jump from the source
+        }
+
+        // recursive from here
+        // @todo concurrence to async queue. this iterate should transform into async queue, but not concurrence
+        /*
+        * return the array contain element lists
+        * */
+        var _temp = items.filter(function (item, index) {
+            var url = item.attribs.href,
+                // reference : http://stackoverflow.com/questions/10003683/javascript-get-number-from-string
+                date = item.children[0].data.replace(/\D+/g, " ").split(" ").slice(0, 3).join("/"); // should jump when unormal info
+
+            // need prevent date get prolem, such as notice which have no relationship with the data
+            // jump from none data source
+            // fixed bug: http://scxx.whfcj.gov.cn/scxxbackstage/whfcj/channels/854_4.html
+            // http://scxx.whfcj.gov.cn/scxxbackstage/whfcj/contents/854/24309.html
+            // 有最新日期，并且抓取到的日期不大于最新日期的时候，跳出循环
+            if (date.split("/").length != 3) date = 0;
+            log("index : ", index);
+
+            // recursive next page and update the collectio
+            if( index + 1 == items.length && new Date(date) > new Date(latest) ){
+                crawler.parseList(URL,function(items, next){
+                    buildUpdateCollection(items, next, latest, _queue)
+                });
+            }
+            return new Date(date) > new Date(latest);
+        });
+
+        _queue = _queue.concat(_temp); // combine the new array data
+
+}
 
 module.exports.checkUpdate = checkUpdate;
 
